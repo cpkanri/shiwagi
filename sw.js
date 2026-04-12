7d57abace465f1343e770ad8bd6db9d9864c6c38
cpkanri <cleanprokanri@gmail.com>
2026-04-12 17:23:30 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+var CACHE_NAME = 'shibaki-cache-v1';
+var urlsToCache = ['./index.html', './manifest.json'];
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(urlsToCache); }));
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) { return response || fetch(event.request); }));
+});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(caches.keys().then(function(keys) { return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })); }));
+});
